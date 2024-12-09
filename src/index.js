da098a86e01a4f5209167e226ee56ffc7f1457c0
milena <milenaabernardi@gmail.com>
2024-12-09 17:08:16 -0300

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./global.css";
+import React from "react";
+import ReactDOMClient from "react-dom/client"; // Correto para React 18
+import { TelaInicial } from "./screens/TelaInicial";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Obtém o elemento com id 'app'
+const app = document.getElementById("app");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Cria a raiz para renderizar a aplicação
+const root = ReactDOMClient.createRoot(app);
+
+// Renderiza o componente TelaInicial dentro do elemento com id 'app'
+root.render(<TelaInicial />);
---
+++ src/screens/TelaInicial.js
@@ -0,0 +1,94 @@
+import React from "react";
+import "./style.css";
+
+export const TelaInicial = () => {
+  return (
+    <div className="tela-inicial">
+      <div className="div">
+        <div className="overlap">
+          <div className="overlap">
+            <img
+              className="logo-sem-fundo-roxo"
+              alt="Logo sem fundo roxo"
+              src="logo.png"
+            />
+
+            <div className="text-wrapper">Bem-vindo ao MusicFeel!</div>
+          </div>
+
+          <img
+            className="boto-menu"
+            alt="Boto menu"
+            src="icone-menu.png"
+          />
+        </div>
+
+        <div className="overlap-group">
+          <div className="text-wrapper-2">Plano Duo</div>
+
+          <div className="text-wrapper-3">Plano Individual</div>
+
+          <div className="text-wrapper-4">Plano Banda</div>
+
+          <div className="text-wrapper-5">Planos do MusicFeel</div>
+
+          <div className="text-wrapper-6">Suporte</div>
+
+          <div className="text-wrapper-7">Links Úteis</div>
+
+          <div className="text-wrapper-8">Empresa</div>
+
+          <div className="text-wrapper-9">Sobre</div>
+
+          <div className="text-wrapper-10">Empregos</div>
+
+          <img
+            className="instagram"
+            alt="Instagram"
+            src="instagram.png"
+          />
+
+          <img
+            className="facebook"
+            alt="Facebook"
+            src="facebook.png"
+          />
+
+          <img
+            className="youtube"
+            alt="Youtube"
+            src="youtube.png"
+          />
+        </div>
+
+        <p className="aqui-a-m-sica-ganha">
+          Aqui, a música ganha vida em suas mãos! Aprenda a tocar instrumentos
+          musicais com lições interativas, dicas práticas e uma comunidade
+          apaixonada por música. Seja você iniciante ou já experiente, temos o
+          conteúdo certo para ajudar a transformar sua paixão pela música em
+          habilidade.
+          <br />
+          Escolha seu instrumento favorito e comece a jornada musical agora
+          mesmo!
+        </p>
+
+        <div className="div-wrapper">
+          <div className="text-wrapper-11">Comece agora</div>
+        </div>
+
+        <div className="overlap-2">
+          <div className="text-wrapper-12">Visualizar todos os planos</div>
+        </div>
+
+        <p className="p">
+          Escolha o plano ideial e aprenda a tocar o instrumento dos seus
+          sonhos. Vários métodos de pagamento. Cancele quando quiser.
+        </p>
+
+        <p className="text-wrapper-13">
+          Planos acessíveis para qualquer situação
+        </p>
+      </div>
+    </div>
+  );
+};
